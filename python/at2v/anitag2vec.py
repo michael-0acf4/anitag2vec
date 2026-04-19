fix: off by one sincos buffer

python/at2v/anitag2vec.py
@@ -81,9 +81,9 @@ class SegmentRoPEMultiheadAttention(nn.Module):
         return self.Wo(out)
 
 class Block(nn.Module):
-    def __init__(self, d_model: int, n_heads: int, max_len: int):
+    def __init__(self, d_model: int, n_heads: int, max_seq_len_for_cache_buffer: int):
         super().__init__()
-        self.attn = SegmentRoPEMultiheadAttention(d_model, n_heads, max_len)
+        self.attn = SegmentRoPEMultiheadAttention(d_model, n_heads, max_seq_len_for_cache_buffer)
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
         self.ffn = nn.Sequential(
@@ -98,10 +98,10 @@ class Block(nn.Module):
         return x
 
 class TransformerStack(nn.Module):
-    def __init__(self, n_layers: int, d_model: int, n_heads: int, max_len: int):
+    def __init__(self, n_layers: int, d_model: int, n_heads: int, max_seq_len_for_cache_buffer: int):
         super().__init__()
         self.blocks = nn.ModuleList([
-            Block(d_model, n_heads, max_len)
+            Block(d_model, n_heads, max_seq_len_for_cache_buffer)
             for _ in range(n_layers)
         ])
 
@@ -127,19 +127,14 @@ class AniTag2Vec(nn.Module):
         self.segmented_rope = segmented_rope
         self.emb = nn.Embedding(num_embeddings=vocab_size + buff, embedding_dim=d_model)
         if self.segmented_rope:
-            # self.transformer = SegmentRoPEMultiheadAttention(
-            #     d_model=d_model,
-            #     n_heads=n_heads,
-            #     # for prepcomputing cos/sin buffers
-            #     # this can be lower actually but since input is 128
-            #     # then each individual tags must have token count smaller than that
-            #     max_seq_len_for_cache_buffer=max_len_cut
-            # )
+            buff = 100
             self.transformer_rope = TransformerStack(
                 n_layers=n_layers,
                 d_model=d_model,
                 n_heads=n_heads,
-                max_len=max_len_cut
+                # longest sequence is 1, 2, .., 128
+                # (max_len_cut + 1) is the minimum cache size
+                max_seq_len_for_cache_buffer=max_len_cut + buff
             )
         else:
             self.transformer = nn.TransformerEncoder(

python/onnx_export.py
@@ -2,11 +2,13 @@ import argparse
 import os
 
 
-def onnx_export(model_path: str, config_path: str):
+def onnx_export(model_path: str, tokenizer_path: str, config_path: str):
     import torch
-    from at2v.anitag2vec import AniTag2Vec, ModelConfig, get_chunked_positions_torch
+    from at2v.anitag2vec import AniTag2Vec, ModelConfig, TagBPETokenizer
     print("Loading PyTorch model..")
-    cfg = ModelConfig.load_from_file(config_path)    
+    cfg = ModelConfig.load_from_file(config_path)
+    tagtok = TagBPETokenizer.load_from_file(tokenizer_path)
+
     anitag2vec = AniTag2Vec(
         vocab_size=cfg.HYPERP_TAGTOK_VOCAB_SIZE,
         max_len_cut=cfg.HYPERP_TAGTOK_MAX_TOKEN_CLAMP,
@@ -14,7 +16,7 @@ def onnx_export(model_path: str, config_path: str):
         n_heads=cfg.HYPERP_TRANSFORMER_N_HEADS,
         n_layers=cfg.HYPERP_TRANSFORMER_N_LAYERS,
         output_emb=cfg.HYPERP_OUTPUT_EMB,
-        encode_split_token_id=cfg.HYPERP_INPUT_ALLOW_POS_ENCODING_TOKEN_ID
+        segmented_rope=cfg.HYPERP_ENABLE_SEGMENTED_ROPE
     )
     anitag2vec.load_state_dict(torch.load(model_path))
     anitag2vec.eval()
@@ -22,27 +24,35 @@ def onnx_export(model_path: str, config_path: str):
     print("Exporting to ONNX...")
     base, _ = os.path.splitext(model_path)
     output_file = f"{base}.onnx"
-    example_input = torch.randint(0, 1000, (2, cfg.HYPERP_TAGTOK_MAX_TOKEN_CLAMP,), dtype=torch.int64)
-    example_pos = get_chunked_positions_torch(example_input, anitag2vec.encode_split_token_id) 
+
+    input_names = ["x"]
+    output_names = ["y"]
+    dynamic_axes = {
+        # "input": { 0: torch.export.Dim("batch") }
+        "x": { 0: "batch" },
+        "y": { 0: "batch" }
+    }
+    x = torch.randint(0, 1000, (2, cfg.HYPERP_TAGTOK_MAX_TOKEN_CLAMP,), dtype=torch.int64)
+    if anitag2vec.segmented_rope:
+        input_names.append("x_chunked_pos")
+        dynamic_axes["x_chunked_pos"] = { 0: "batch" }
+        example_input = (x, tagtok.get_chunked_positions_torch(x))
+    else:
+        example_input = (x,)
     # example_input = torch.randn((1, cfg.HYPERP_TAGTOK_MAX_TOKEN_CLAMP,), dtype=torch.int64)
     # anitag2vec(example_input)
     torch.onnx.export(
         anitag2vec,
-        (example_input, example_pos),
+        example_input,
         output_file,
         # dynamo=False,
         training=torch.onnx.TrainingMode.EVAL,
         opset_version=14,
         # do_constant_folding=False,
-        input_names=["x", "x_chunked_pos"],
-        output_names=["y"],
+        input_names=input_names,
+        output_names=output_names,
         # dynamic_shapes={
-        dynamic_axes={
-            # "input": { 0: torch.export.Dim("batch") }
-            "x": { 0: "batch" },
-            "x_chunked_pos": { 0: "batch" },
-            "y": { 0: "batch" }
-        },
+        dynamic_axes=dynamic_axes,
         # verbose=True
     )
 
@@ -60,13 +70,19 @@ def main():
         help="Model file .pt, .pth path"
     )
     parser.add_argument(
+        "--tokenizer",
+        type=str,
+        required=True,
+        help="Tokenizer model json config path"
+    )
+    parser.add_argument(
         "--config",
         type=str,
         required=True,
         help="Model json config path"
     )
     args = parser.parse_args()
-    onnx_export(args.model, args.config)
+    onnx_export(args.model, args.tokenizer, args.config)
 
 if __name__ == "__main__":
     main()